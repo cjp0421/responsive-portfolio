aeaafb589db46b16a7748f6f28657c5775dd55b9
cjp0421 <pedersencarol1@gmail.com>
2022-02-14 15:50:25 -0600

    building html and js for new section of website; uploaded image for section

---
+++ learningProof.js
@@ -0,0 +1,12 @@
+const images = [
+  {
+    image: ".images/caroselImages/introUX.png",
+    title: "Introduction to User Experience Design Certificate",
+    url: "https://coursera.org/share/b8ee3df11f5d5966bf8d217106a1b2c3",
+  },
+  {
+    image: ".images/caroselImages/introUX.png",
+    title: "Introduction to User Experience Design Certificate",
+    url: "https://coursera.org/share/b8ee3df11f5d5966bf8d217106a1b2c3",
+  },
+];
