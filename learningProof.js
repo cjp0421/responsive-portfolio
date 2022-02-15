d1326d40c86bb4640edde6ca8b33a46d8eccd773
cjp0421 <pedersencarol1@gmail.com>
2022-02-15 13:37:40 -0600

    changed css, html, and js for testing concepts
    experimenting with carousel types for new page

--- learningProof.js
+++ learningProof.js
@@ -1,12 +1,32 @@
-const images = [
-  {
-    image: ".images/caroselImages/introUX.png",
-    title: "Introduction to User Experience Design Certificate",
-    url: "https://coursera.org/share/b8ee3df11f5d5966bf8d217106a1b2c3",
-  },
-  {
-    image: ".images/caroselImages/introUX.png",
-    title: "Introduction to User Experience Design Certificate",
-    url: "https://coursera.org/share/b8ee3df11f5d5966bf8d217106a1b2c3",
-  },
-];
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
